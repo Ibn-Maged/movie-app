cf5f4df1a98ec8039e6d2b66b7d573bbd01016b8
Ibn-Maged <mohamedmaged1607@gmail.com>
2024-08-21 11:22:17 +0300

    added stylings for details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,44 @@
+import { useContext } from "react";
 import { useParams } from "react-router-dom"
+import { moviesContext } from "../contexts/MoviesContextProvider";
+import star from "../assets/star.svg"
+import "../styles/details.css"
+const img_path = "https://image.tmdb.org/t/p/w500/";
 
 const Details = () => {
   const params = useParams();
+  const {movies, movieGenres} = useContext(moviesContext);
+  const currentMovie = movies[params.id];
   return (
-    <div>Details of {params.id}</div>
+    <div className="d-flex col-12 flex-wrap gap-5">
+      <div className="col-lg-4">
+        <img src={img_path + currentMovie.poster_path} alt="" className="poster-image img-fluid"/>
+      </div>
+      <div className="d-flex flex-column col-lg-7">
+        <div className="d-flex justify-content-between align-items-center">
+          <h1>{currentMovie.title}</h1>
+          <div className="secondary-text">
+            <img src={star} alt="star" className="star-icon"/> 
+            {currentMovie.vote_average.toFixed(1)} / 10
+          </div>
+        </div>
+        <div className="secondary-text mb-5">
+          {currentMovie.release_date}
+        </div>
+        <p className="overview-text">
+          {currentMovie.overview}
+        </p>
+        <div className="d-flex">
+          {currentMovie.genre_ids.map(id=>{
+            for(let i = 0; i < movieGenres.length; ++i){
+              if(movieGenres[i].id == id){
+                return <div className="me-4 btn btn-outline-secondary genre">{movieGenres[i].name}</div>;
+              }
+            }
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
 
